5c2dfcd7faf650adaf11ac98bcb04137b6913056
AMV_Ph34r <AMVPh34r@gmail.com>
2016-11-01 18:19:07 -0400

    Bump version number

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ import logging
 from plugin_manager import PluginManager
 
 APP_NAME = "NaNoBot"
-APP_VERSION = "1.0.4"
+APP_VERSION = "1.1.0"
 APP_AUTHOR = "Alex Schaeffer"
 
 log = logging.getLogger('discord')
