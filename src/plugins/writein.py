593d56b33d2a86b133b1135b6049c28cefc50ae9
AMV_Ph34r <AMVPh34r@gmail.com>
2016-11-01 17:22:11 -0400

    Add base Writein plugin
    No leaderboard yet but starting, finishing and submitting word counts
    have been implemented

---
+++ src/plugins/writein.py
@@ -0,0 +1,161 @@
+from plugin import Plugin
+from decorators import command
+
+
+class WriteinObj(object):
+    channel = None
+    started_by = None
+    is_active = False
+    wordcounts = {}
+
+    def __init__(self, channel, started_by):
+        self.channel = channel
+        self.started_by = started_by
+
+    async def start(self):
+        self.is_active = True
+
+    async def finish(self):
+        self.is_active = False
+
+    async def submit_wordcount(self, user, count):
+        if self.is_active:
+            self.wordcounts[user.id] = count
+        return self.is_active
+
+    async def get_wordcount(self, user):
+        # print(self.wordcounts)
+        if user.id in self.wordcounts.keys():
+            return self.wordcounts[user.id]
+        else:
+            return None
+
+    async def leaderboard(self):
+        return sorted(self.wordcounts.items(), key=lambda x: x[1])
+
+
+class Writein(Plugin):
+    fancy_name = "Writeins"
+
+    writeins = {}
+
+    @staticmethod
+    async def get_commands(server):
+        commands = [
+            {
+                'name': '!writein start',
+                'description': 'Begin a writein.'
+            },
+            {
+                'name': '!writein report [x]',
+                'description': 'Report your wordcount for the writein.'
+            },
+            {
+                'name': '!writein check',
+                'description': 'Check your current submitted wordcount.'
+            },
+            {
+                'name': '!writein leaderboard',
+                'description': 'Show a leaderboard of wordcounts for the current writein.'
+            },
+            {
+                'name': '!writein finish',
+                'description': 'Complete the writein. Only the writein starter can use this command.'
+            }
+        ]
+        return commands
+
+    async def get_writein(self, channel):
+        if channel in self.writeins.keys():
+            timer = self.writeins[channel]
+            if timer.is_active:
+                return timer
+            else:
+                del self.writeins[channel]
+        return None
+
+    @command(pattern='^!writein start$')
+    async def writein_start(self, message, args):
+        writein = await self.get_writein(message.channel)
+        if writein:
+            response = "There is already a writein in progress, started by {started_by}.\n" \
+                       "To complete it, {started_by} can issue the command `!writein finish`.".format(
+                            started_by=writein.started_by.name
+                        )
+            await self.bot.send_message(message.channel, response)
+            return
+
+        del writein
+        writein = WriteinObj(message.channel, message.author)
+        await writein.start()
+        self.writeins[message.channel] = writein
+        response = "{} started a writein! Let's get writing!".format(message.author.name)
+
+        await self.bot.send_message(message.channel, response)
+
+    @command(pattern='^!writein report ([0-9]*)$')
+    async def writein_report(self, message, args):
+        count = args[0]
+        writein = await self.get_writein(message.channel)
+        if not writein or not writein.is_active:
+            response = "There's currently no active writein.\n" \
+                       "To start one, send `!writein start`."
+            await self.bot.send_message(message.channel, response)
+            return
+
+        await writein.submit_wordcount(message.author, count)
+        response = "{}: Successfully updated your wordcount for this writein to {}!".format(
+            message.author.mention, count
+        )
+        await self.bot.send_message(message.channel, response)
+
+    @command(pattern='^!writein check$')
+    async def writein_check(self, message, args):
+        writein = await self.get_writein(message.channel)
+        if not writein or not writein.is_active:
+            response = "There's currently no active writein.\n" \
+                       "To start one, send `!writein start`."
+            await self.bot.send_message(message.channel, response)
+            return
+
+        count = await writein.get_wordcount(message.author)
+        if count is None:
+            response = "{}, you haven't submitted your wordcount yet! Do so with `!writein report [count]`.".format(
+                message.author.mention
+            )
+        else:
+            response = "{}, your current wordcount is {}".format(
+                message.author.mention, count
+            )
+        await self.bot.send_message(message.channel, response)
+
+    @command(pattern='^!writein leaderboard$')
+    async def leaderbaord(self, message, args):
+        writein = await self.get_writein(message.channel)
+        if not writein or not writein.is_active:
+            response = "There's currently no active writein.\n" \
+                       "To start one, send `!writein start`."
+            await self.bot.send_message(message.channel, response)
+            return
+
+        response = "Here's the leaderboard for the current writein:"
+        await self.bot.send_message(message.channel, response)
+
+    @command(pattern='^!writein finish$')
+    async def writein_finish(self, message, args):
+        writein = await self.get_writein(message.channel)
+        if not writein or not writein.is_active:
+            response = "There's currently no active writein.\n" \
+                       "To start one, send `!writein start`."
+            await self.bot.send_message(message.channel, response)
+            return
+        if message.author.id != writein.started_by.id:
+            response = "Sorry, this writein was started by {}, only they may cancel it".format(
+                writein.started_by.mention
+            )
+            await self.bot.send_message(message.channel, response)
+            return
+        await writein.finish()
+        response = "Writein complete! Here is the final leaderboard:\n{}"
+
+        await self.bot.send_message(message.channel, response)
