9c15f39fc8d7238da86d44244dae2c359250db7c
AMV_Ph34r <AMVPh34r@gmail.com>
2016-11-03 11:27:19 -0400

    Add !wordhistory command

--- src/plugins/nanowrimo_api.py
+++ src/plugins/nanowrimo_api.py
@@ -1,3 +1,4 @@
+import datetime
 import xml.etree.ElementTree as ET
 import aiohttp
 from plugin import Plugin
@@ -13,6 +14,10 @@ class NaNoWriMoAPI(Plugin):
             {
                 'name': '!wordcount [username]',
                 'description': 'Show the current wordcout for NaNo site user `[username]`.'
+            },
+            {
+                'name': '!wordhistory [username]',
+                'description': 'Show recent wordcount history for NaNo site user `[username]`.'
             }
         ]
         return commands
@@ -20,9 +25,13 @@ class NaNoWriMoAPI(Plugin):
     @command(pattern='^!wordcount (.*)$')
     async def wordcount(self, message, args):
         username = args[0]
-        url = "http://nanowrimo.org/wordcount_api/wc/{}".format(username)
+        api_url = "http://nanowrimo.org/wordcount_api/wc/{}".format(username)
+        prof_url_template = "http://nanowrimo.org/participants/{}"
+        response_template = "{name} has written {count} words so far!\n" \
+                            "{name}'s profile: {url}"
+
         with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
+            async with session.get(api_url) as resp:
                 resp_text = await resp.text()
                 root = ET.fromstring(resp_text)
         if root.find('error') is not None:
@@ -30,13 +39,54 @@ class NaNoWriMoAPI(Plugin):
                 root.find('error').text
             )
         else:
-            response = "{name} has written {count} words so far!\n" \
-                       "{name}'s profile: {url}".format(
+            response = response_template.format(
                             name=root.find('uname').text,
                             count='{:,}'.format(
                                 int(root.find('user_wordcount').text)
                             ),
-                            url="http://nanowrimo.org/participants/{}".format(root.find('uname').text)
+                            url=prof_url_template.format(root.find('uname').text)
                         )
+
+        await self.bot.send_message(message.channel, response)
+
+    @command(pattern='^!wordhistory (.*)$')
+    async def wordcount_history(self, message, args):
+        username = args[0]
+        api_url = "http://nanowrimo.org/wordcount_api/wchistory/{}".format(username)
+        prof_url_template = "http://nanowrimo.org/participants/{}"
+        response_template = "{name}'s recent word history:\n{history}" \
+                            "Total words to date: `{count}`\n\n" \
+                            "{name}'s profile: {url}"
+        history_template = "{hdate}: `{hcount}`\n"
+        history_str = ""
+
+        with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as resp:
+                resp_text = await resp.text()
+                root = ET.fromstring(resp_text)
+        if root.find('error') is not None:
+            response = "Error: {}".format(
+                root.find('error').text
+            )
+        else:
+            for entry in root.find('wordcounts')[-5:]:
+                history_str += history_template.format(
+                    hdate=datetime.datetime.strptime(
+                        entry.find('wcdate').text,
+                        '%Y-%m-%d'
+                    ).strftime('%B %d'),
+                    hcount='{:,}'.format(
+                        int(entry.find('wc').text)
+                    )
+                )
+
+            response = response_template.format(
+                name=root.find('uname').text,
+                count='{:,}'.format(
+                    int(root.find('user_wordcount').text)
+                ),
+                history=history_str,
+                url=prof_url_template.format(root.find('uname').text)
+            )
 
         await self.bot.send_message(message.channel, response)
